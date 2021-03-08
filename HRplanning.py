6211e2122d988d852866ad1f95fe3a8e06ca375d
esteban126505 <78742476+esteban126505@users.noreply.github.com>
2021-03-08 16:17:50 +0100

    Update HRplanning.py

--- HRplanning.py
+++ HRplanning.py
@@ -5,7 +5,8 @@ D = numpy.array([[15, 0, 0],[0, 20, 0],[0, 0, 50]])
 M = numpy.array([[5, 2, 0],[0, 10, 0],[5, 5, 45]])
 b = numpy.array([[3],[1],[1]])
 a = M@numpy.linalg.inv(D)@b
-print(a)
+print(a)([[1.1],[0.5],[2.15]])
+At the beginning we were three managers, one operator and one engineer. The predictions of the number of available workers are: one manager, one engineer and twpo operators.
 
 # Forecasting number of needed workers
 X = numpy.array([[],[],[],[],[],[],[],[],[]])
